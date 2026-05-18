ee037c4cd6e8fb4bc85a83cc5a65d354ea1a19ed
Shrivathsa M S <shrivathsa.ms@solvendo.io>
2026-05-18 21:01:35 +0530

    Site name update

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Vathsa Has Opinions';
-export const SITE_DESCRIPTION = 'Welcome to my site/blog/portfolio/rants';
+export const SITE_TITLE = 'Flow Field';
+export const SITE_DESCRIPTION = "Welcome to Vathsa's site/blog/portfolio/rants";
 export const AUTOPLAY = false;
